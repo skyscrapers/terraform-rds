bb5e4506f7e5b3214e9a0c3fe751644e08ffabbf
Luca Venturelli <luca@skyscrapers.eu>
2017-08-09 16:23:51 +0200

    fixing subnet creation

--- rds-replica/main.tf
+++ rds-replica/main.tf
@@ -1,3 +1,10 @@
+resource "aws_db_subnet_group" "rds" {
+  count       = "${length(var.subnets) > 0 ? 1:0}"
+  name        = "replica-${var.environment}${var.name}-rds"
+  description = "The group of subnets"
+  subnet_ids  = ["${var.subnets}"]
+}
+
 resource "aws_db_instance" "rds" {
   identifier                = "replica-${var.environment}-${var.name}-rds${var.number}"
   engine                    = "${var.engine}"
@@ -6,6 +13,7 @@ resource "aws_db_instance" "rds" {
   replicate_source_db       = "${var.replicate_source_db}"
   availability_zone         = "${var.availability_zone}"
   final_snapshot_identifier = "${var.project}-${var.environment}${var.name}-rds${var.number}-final-${md5(timestamp())}"
+  db_subnet_group_name      = "${length(var.subnets) > 0? aws_db_subnet_group.rds.id : ""}"
 
   tags {
     Name        = "replica-${var.environment}-${var.name}-rds${var.number}"
