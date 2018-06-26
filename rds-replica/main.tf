11ffc1bb3bcb281e00bcf853c2a7f175c43bf8a4
Luca Venturelli <luca@skyscrapers.eu>
2018-06-26 07:13:00 +0200

    fixing typos

--- rds-replica/main.tf
+++ rds-replica/main.tf
@@ -1,20 +1,20 @@
 resource "aws_db_subnet_group" "rds" {
-  tag        = "replica-${var.environment}${var.tag}-rds"
+  name        = "${length(var.name)==0 ? "${var.project}-${var.environment}${var.tag}-rds${var.number}-replica" : var.name}"
   description = "The group of subnets"
   subnet_ids  = ["${var.subnets}"]
 }
 
 resource "aws_db_instance" "rds" {
-  identifier                = "${var.project}-${var.environment}${var.tag}-rds${var.number}-replica"
+  identifier                = "${length(var.name)==0 ? "${var.project}-${var.environment}${var.tag}-rds${var.number}-replica" : var.name}"
   engine                    = "${var.engine}"
   instance_class            = "${var.size}"
   vpc_security_group_ids    = ["${aws_security_group.sg_rds.id}"]
   replicate_source_db       = "${var.replicate_source_db}"
   final_snapshot_identifier = "${var.project}-${var.environment}${var.tag}-rds${var.number}-final-${md5(timestamp())}"
-  db_subnet_group_tag      = "${aws_db_subnet_group.rds.id}"
+  db_subnet_group_name      = "${aws_db_subnet_group.rds.id}"
 
   tags {
-    tag        = "${var.project}-${var.environment}${var.tag}-rds${var.number}-replica"
+    tag        = "${length(var.name)==0 ? "${var.project}-${var.environment}${var.tag}-rds${var.number}-replica" : var.name}"
     Environment = "${var.environment}"
     Project     = "${var.project}"
   }
