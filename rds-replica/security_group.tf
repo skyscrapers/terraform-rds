af16b7e3c81981e59b14756c2cd993484c1e27ad
Wouter Reyntjens <wouter@chismoor.net>
2018-07-26 15:19:31 +0200

    changing SG name (#22)
    * changing SG name
    
    * changing SG name
    
    * changing SG name

--- rds-replica/security_group.tf
+++ rds-replica/security_group.tf
@@ -8,12 +8,12 @@ variable "ports" {
 
 # Create RDS with Subnet and paramter group,
 resource "aws_security_group" "sg_rds" {
-  name        = "sg_rds_${var.project}_${var.environment}${var.name}"
-  description = "Security group that is needed for the RDS"
+  name        = "sg_rds_replica_${var.project}_${var.environment}${var.name}"
+  description = "Security group that is needed for the RDS replica"
   vpc_id      = "${var.vpc_id}"
 
   tags {
-    Name        = "${var.project}-${var.environment}${var.name}-sg_rds"
+    Name        = "${var.project}-${var.environment}${var.name}-sg_rds_replica"
     Environment = "${var.environment}"
     Project     = "${var.project}"
   }
