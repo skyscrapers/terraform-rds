bfb00bf22b55c110e1c11040a6c353375f06080f
Wouter Reyntjens <wouter@skyscrapers.eu>
2018-09-25 11:44:11 +0200

    Cleaning up code

--- snapshot-replicator/functions/create_snapshot.py
+++ snapshot-replicator/functions/create_snapshot.py
@@ -1,22 +1,14 @@
 import boto3  
-import datetime  
-import re
+import datetime 
 import os
 
-SOURCE_REGION = os.environ['SOURCE_REGION']
+source_region = os.environ['SOURCE_REGION']
 instances = os.environ['DB_INSTANCES']
 iam = boto3.client('iam')
 print('Loading function')
 
 def lambda_handler(event, context):  
-    account_ids = []
-    try:
-        iam.get_user()
-    except Exception as e:
-        account_ids.append(re.search(r'(arn:aws:sts::)([0-9]+)', str(e)).groups()[1])
-        account = account_ids[0]
-
-    source = boto3.client('rds', region_name=SOURCE_REGION)
+    source = boto3.client('rds', region_name=source_region)
 
     for instance in instances.split(','):
         now = datetime.datetime.now()
