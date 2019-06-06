622baa984b354d97432eef0dcf24b4bb73e52cb4
Luca Venturelli <luca@skyscrapers.eu>
2019-06-06 05:10:09 -0500

    adding aurora and rds changes to move to tf12

--- aurora/security_group.tf
+++ aurora/security_group.tf
@@ -10,12 +10,12 @@ locals {
 resource "aws_security_group" "sg_aurora" {
   name        = "sg_aurora_${var.project}_${var.environment}${var.tag}"
   description = "Security group that is needed for the Aurora"
-  vpc_id      = "${data.aws_subnet.subnet.vpc_id}"
+  vpc_id      = data.aws_subnet.subnet.vpc_id
 
   tags {
     Name        = "${var.project}-${var.environment}${var.tag}-sg_aurora"
-    Environment = "${var.environment}"
-    Project     = "${var.project}"
+    Environment = var.environment
+    Project     = var.project
   }
 
   lifecycle {
@@ -24,11 +24,11 @@ resource "aws_security_group" "sg_aurora" {
 }
 
 resource "aws_security_group_rule" "sg_aurora_in" {
-  count                    = "${length(var.security_groups)}"
-  security_group_id        = "${aws_security_group.sg_aurora.id}"
+  count                    = length(var.security_groups)
+  security_group_id        = aws_security_group.sg_aurora.id
   type                     = "ingress"
-  from_port                = "${local.port}"
-  to_port                  = "${local.port}"
+  from_port                = local.port
+  to_port                  = local.port
   protocol                 = "tcp"
-  source_security_group_id = "${element(var.security_groups, count.index)}"
+  source_security_group_id = element(var.security_groups, count.index)
 }
