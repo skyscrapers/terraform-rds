eb42f9f926da36385d8bb9913846325038139277
Amany Elguindy <minniux@gmail.com>
2019-10-10 16:23:01 +0200

    use less for/if

--- snapshot-replicator/functions/remove_snapshots.py
+++ snapshot-replicator/functions/remove_snapshots.py
@@ -17,20 +17,19 @@ def lambda_handler(event, context):
         for instance in instances.split(','):
             rds = boto3.client('rds', region_name=region)
             paginator = rds.get_paginator('describe_db_snapshots')
-            page_iterator = paginator.paginate(DBInstanceIdentifier=instance)
+            page_iterator = paginator.paginate(DBInstanceIdentifier=instance, SnapshotType='manual')
             snapshots = []
             for page in page_iterator:
                  snapshots.extend(page['DBSnapshots'])
             for snapshot in snapshots:
-                if snapshot['SnapshotType'] == 'manual':
-                    create_ts = snapshot['SnapshotCreateTime'].replace(tzinfo=None)
-                    if create_ts < datetime.datetime.now() - datetime.timedelta(days=int(duration)):
-                        print("Deleting snapshot id:", snapshot['DBSnapshotIdentifier'])
-                        try:
-                            response = rds.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
-                            print response
-                        except botocore.exceptions.ClientError as e:
-                            raise Exception("Could not issue delete command: %s" % e)
+                create_ts = snapshot['SnapshotCreateTime'].replace(tzinfo=None)
+                if create_ts < datetime.datetime.now() - datetime.timedelta(days=int(duration)):
+                    print("Deleting snapshot id:", snapshot['DBSnapshotIdentifier'])
+                    try:
+                        response = rds.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
+                        print response
+                    except botocore.exceptions.ClientError as e:
+                        raise Exception("Could not issue delete command: %s" % e)
 
     deleteSnapshots(region=source_region)
     deleteSnapshots(region=target_region)
--- snapshot-replicator/functions/shipper.py
+++ snapshot-replicator/functions/shipper.py
@@ -15,20 +15,19 @@ def lambda_handler(event, context):
         source = boto3.client('rds', region_name=source_region)
         source_snap = event['Records'][0]['Sns']['Source']
         snapshot_details = source.describe_db_snapshots(DBSnapshotIdentifier=source_snap)['DBSnapshots'][0]
-        for instance in instances.split(','):
-            if instance in snapshot_detailts['DBInstanceIdentifier']:
-                source_snap_arn = snapshot_detailts['DBSnapshotArn'])
-                target_snap_id = (re.sub('rds:', '', source_snap))
-                target = boto3.client('rds', region_name=target_region)
-                print('Will Copy %s to %s' % (source_snap_arn, target_snap_id))
-                try:
-                    response = target.copy_db_snapshot(
-                    SourceDBSnapshotIdentifier=source_snap_arn,
-                    TargetDBSnapshotIdentifier=target_snap_id,
-                    SourceRegion=source_region,
-                    KmsKeyId=kms_key_id,
-                    CopyTags = True)
-                    print(response)
-                except botocore.exceptions.ClientError as e:
-                    raise Exception("Could not issue copy command: %s" % e)
+        if snapshot_detailts['DBInstanceIdentifier'] in instances.split(','):
+            source_snap_arn = snapshot_detailts['DBSnapshotArn'])
+            target_snap_id = (re.sub('rds:', '', source_snap))
+            target = boto3.client('rds', region_name=target_region)
+            print('Will Copy %s to %s' % (source_snap_arn, target_snap_id))
+            try:
+                response = target.copy_db_snapshot(
+                SourceDBSnapshotIdentifier=source_snap_arn,
+                TargetDBSnapshotIdentifier=target_snap_id,
+                SourceRegion=source_region,
+                KmsKeyId=kms_key_id,
+                CopyTags = True)
+                print(response)
+            except botocore.exceptions.ClientError as e:
+                raise Exception("Could not issue copy command: %s" % e)
                 
