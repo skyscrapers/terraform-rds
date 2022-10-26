c671b345bbf2ec2dc49257f708ba8d4086e8c75a
iuri aranda <iuri@skyscrapers.eu>
2022-10-26 14:27:26 +0200

    Fix snapshot cross-account replicator cleanup (#62)
    * Fix snapshot cross-account replicator cleanup
    
    Decouple copy and replication functions from cleanup tasks. Now there'll be three cleanup functions that periodically will remove old snapshots. Intermediate snapshots will be deleted after the retention period + a safe buffer of 2 days. The safe buffer is to account for shorter retention periods and large snapshots, to give them time to get fully replicated. If a snapshot hasn't been replicated after 2 days, we assume it will never be.
    
    As per https://github.com/skyscrapers/engineering/issues/835
    
    * Use safe_period to clean up intermediate snapshots
    
    * Shorten lambda function names
    
    * Fix intermediate cleanup lambda sns topic
    
    * Change the snapshot period variable format
    
    To avoid manual mistakes when setting up the monitoring period, we'll use a simple rate expression to set the snapshot period, in hours, and automatically set the monitoring period to the same value (plus 1 hour).
    
    Before the monitoring period could easily be missed, and if set to something lower than the snapshot period, could cause missed alerts.
    
    * Fix cleanup period format in lambda

--- snapshot-cross-account-replicator/lambda_target_account.tf
+++ snapshot-cross-account-replicator/lambda_target_account.tf
@@ -1,56 +1,12 @@
-## Step 4 Lambda function:
-##  Triggered when the final snapshot in the replica AWS account has finished copying,
-##  this function will delete the intermediate snapshot created in step 2.
-
-resource "aws_lambda_function" "step_4" {
-  provider         = aws.target
-  function_name    = "rds-snapshots-replicator-step-4-${var.name}"
-  role             = aws_iam_role.target_lambda.arn
-  handler          = "lambda.delete_intermediate_snapshot"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "python3.9"
-  architectures    = ["arm64"]
-  timeout          = "120"
-
-  environment {
-    variables = local.lambda_default_environment_variables
-  }
-}
-
-## This event will be triggered when the final snapshot has been copied to the target account
-## so the intermediate snapshot can be safely deleted
-resource "aws_cloudwatch_event_rule" "invoke_step_4_lambda" {
-  provider            = aws.target
-  description         = "Triggers lambda function ${aws_lambda_function.step_4.function_name}"
-  event_pattern       = !var.is_aurora_cluster ? local.invoke_step_4_lambda_event_pattern_instance : null
-  schedule_expression = var.is_aurora_cluster ? "cron(*/30 * * * ? *)" : null
-}
-
-resource "aws_cloudwatch_event_target" "invoke_step_4_lambda" {
-  provider = aws.target
-  rule     = aws_cloudwatch_event_rule.invoke_step_4_lambda.name
-  arn      = aws_lambda_function.step_4.arn
-}
-
-resource "aws_lambda_permission" "invoke_step_4_lambda" {
-  provider      = aws.target
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.step_4.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.invoke_step_4_lambda.arn
-}
-
-## Cleanup Lambda function:
+## Cleanup target account Lambda function:
 ##  Runs periodically to delete old snapshots from the replica account,
 ##  based on the configured retention period.
 
-resource "aws_lambda_function" "cleanup_snapshots" {
+resource "aws_lambda_function" "cleanup_target" {
   provider         = aws.target
   function_name    = "rds-snapshots-replicator-cleanup-${var.name}"
   role             = aws_iam_role.target_lambda.arn
-  handler          = "lambda.cleanup_snapshots"
+  handler          = "lambda.cleanup_final_snapshots"
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.9"
@@ -62,23 +18,23 @@ resource "aws_lambda_function" "cleanup_snapshots" {
   }
 }
 
-resource "aws_cloudwatch_event_rule" "invoke_cleanup_snapshots_lambda" {
+resource "aws_cloudwatch_event_rule" "invoke_cleanup_target_lambda" {
   provider            = aws.target
-  description         = "Triggers lambda function ${aws_lambda_function.cleanup_snapshots.function_name}"
+  description         = "Triggers lambda function ${aws_lambda_function.cleanup_target.function_name}"
   schedule_expression = "cron(0 3 * * ? *)"
 }
 
-resource "aws_cloudwatch_event_target" "invoke_cleanup_snapshots_lambda" {
+resource "aws_cloudwatch_event_target" "invoke_cleanup_target_lambda" {
   provider = aws.target
-  rule     = aws_cloudwatch_event_rule.invoke_cleanup_snapshots_lambda.name
-  arn      = aws_lambda_function.cleanup_snapshots.arn
+  rule     = aws_cloudwatch_event_rule.invoke_cleanup_target_lambda.name
+  arn      = aws_lambda_function.cleanup_target.arn
 }
 
-resource "aws_lambda_permission" "invoke_cleanup_snapshots_lambda" {
+resource "aws_lambda_permission" "invoke_cleanup_target_lambda" {
   provider      = aws.target
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.cleanup_snapshots.function_name
+  function_name = aws_lambda_function.cleanup_target.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.invoke_cleanup_snapshots_lambda.arn
+  source_arn    = aws_cloudwatch_event_rule.invoke_cleanup_target_lambda.arn
 }
--- snapshot-cross-account-replicator/monitoring.tf
+++ snapshot-cross-account-replicator/monitoring.tf
@@ -9,12 +9,15 @@ locals {
     module.step_3_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
     module.step_3_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
     module.step_3_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn,
-    module.step_4_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
-    module.step_4_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
-    module.step_4_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn,
-    module.cleanup_snapshots_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
-    module.cleanup_snapshots_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
-    module.cleanup_snapshots_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn
+    module.cleanup_source_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
+    module.cleanup_source_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
+    module.cleanup_source_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn,
+    module.cleanup_intermediate_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
+    module.cleanup_intermediate_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
+    module.cleanup_intermediate_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn,
+    module.cleanup_target_lambda_monitoring.lambda_invocation_errors_cloudwatch_alarm_arn,
+    module.cleanup_target_lambda_monitoring.lambda_throttles_cloudwatch_alarm_arn,
+    module.cleanup_target_lambda_monitoring.lambda_iterator_age_cloudwatch_alarm_arn
   ]
 }
 
@@ -84,13 +87,19 @@ resource "aws_sns_topic_policy" "target_region_topic" {
   policy   = data.aws_iam_policy_document.target_retion_sns_policy.json
 }
 
+locals {
+  lambda_invocation_error_period_initial = (var.snapshot_schedule_period + 1) * 60 * 60             # 1 hour more than the snapshot period
+  lambda_invocation_error_period         = min(local.lambda_invocation_error_period_initial, 86400) # can be maximum a day
+}
+
 module "step_1_lambda_monitoring" {
-  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.0.1"
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
   lambda_function                            = aws_lambda_function.step_1.function_name
   sns_topic_arn                              = aws_sns_topic.source_region_topic.arn
   lambda_invocation_error_threshold          = 2
-  lambda_invocation_error_period             = var.lambda_monitoring_metric_period
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
   lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
 
   providers = {
     aws = aws.source
@@ -98,12 +107,13 @@ module "step_1_lambda_monitoring" {
 }
 
 module "step_2_lambda_monitoring" {
-  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.0.1"
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
   lambda_function                            = aws_lambda_function.step_2.function_name
   sns_topic_arn                              = aws_sns_topic.source_region_topic.arn
   lambda_invocation_error_threshold          = 2
-  lambda_invocation_error_period             = var.lambda_monitoring_metric_period
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
   lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
 
   providers = {
     aws = aws.source
@@ -111,38 +121,55 @@ module "step_2_lambda_monitoring" {
 }
 
 module "step_3_lambda_monitoring" {
-  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.0.1"
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
   lambda_function                            = aws_lambda_function.step_3.function_name
   sns_topic_arn                              = aws_sns_topic.target_region_topic.arn
   lambda_invocation_error_threshold          = 2
-  lambda_invocation_error_period             = var.lambda_monitoring_metric_period
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
   lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
 
   providers = {
     aws = aws.intermediate
   }
 }
 
-module "step_4_lambda_monitoring" {
-  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.0.1"
-  lambda_function                            = aws_lambda_function.step_4.function_name
-  sns_topic_arn                              = aws_sns_topic.target_region_topic.arn
+module "cleanup_source_lambda_monitoring" {
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
+  lambda_function                            = aws_lambda_function.cleanup_source.function_name
+  sns_topic_arn                              = aws_sns_topic.source_region_topic.arn
   lambda_invocation_error_threshold          = 2
-  lambda_invocation_error_period             = var.lambda_monitoring_metric_period
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
   lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
 
   providers = {
-    aws = aws.target
+    aws = aws.source
   }
 }
 
-module "cleanup_snapshots_lambda_monitoring" {
-  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.0.1"
-  lambda_function                            = aws_lambda_function.cleanup_snapshots.function_name
+module "cleanup_intermediate_lambda_monitoring" {
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
+  lambda_function                            = aws_lambda_function.cleanup_intermediate.function_name
   sns_topic_arn                              = aws_sns_topic.target_region_topic.arn
   lambda_invocation_error_threshold          = 2
-  lambda_invocation_error_period             = var.lambda_monitoring_metric_period
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
   lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
+
+  providers = {
+    aws = aws.intermediate
+  }
+}
+
+module "cleanup_target_lambda_monitoring" {
+  source                                     = "github.com/skyscrapers/terraform-cloudwatch//lambda_function?ref=2.1.0"
+  lambda_function                            = aws_lambda_function.cleanup_target.function_name
+  sns_topic_arn                              = aws_sns_topic.target_region_topic.arn
+  lambda_invocation_error_threshold          = 2
+  lambda_invocation_error_period             = local.lambda_invocation_error_period
+  lambda_invocation_error_evaluation_periods = 1
+  lambda_invocation_error_treat_missing_data = "breaching"
 
   providers = {
     aws = aws.target
