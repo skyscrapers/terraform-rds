4896ffc42b66d1eec464284d3f1d0f05a974f0ae
Vahan Terzibashian <vahan8@gmail.com>
2024-05-22 14:38:27 +0400

    expose more variables and offer the option of enabling the reader_endpoint

--- rds-proxy/outputs.tf
+++ rds-proxy/outputs.tf
@@ -2,3 +2,7 @@ output "proxy_endpoint" {
   description = "Endpoint of the created proxy"
   value       = aws_db_proxy.proxy.endpoint
 }
+output "proxy_reader_endpoint" {
+  description = "Reader endpoint of the created proxy"
+  value       = length(aws_db_proxy_endpoint.proxy_reader_endpoint) > 0 ? aws_db_proxy_endpoint.proxy_reader_endpoint[0].endpoint : ""
+}
